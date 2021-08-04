ad106aa4326b02ddc8659d52f673ab7bfcda0def
Lemon-zeniye <endalklemon.abate@gmail.com>
2021-08-04 06:10:30 -0700

    city tours practice

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,44 +1,11 @@
-import { useState, useEffect } from "react";
-import Header from "./Header";
-import Form from "./Form";
-import TodoList from "./TodoList";
-import Footer from "./Footer";
+import Navbar from "./Navbar";
+import CityList from "./CityList";
 import "../App.css";
 const App = () => {
-    const [todos, setTodos] = useState([]);
-    const [status, setStatus] = useState("all");
-    const [filtters, setFiltters] = useState([]);
-    const addTodos = (inputText) => {
-        setTodos([...todos, inputText])
-    }
-    const [toggler, setToggler] = useState(false);
-    const filterHandeler = () => {
-        switch(status){
-            case "completed":
-                setFiltters(todos.filter(todo => todo.completed));
-            break
-            case "uncompleted":
-                setFiltters(todos.filter(todo => !todo.completed ))
-            break
-            case "all":
-                setFiltters(todos.filter(todo => todos));
-        }
-    }
-    const selectHandeler = (e) => {
-      setStatus(e.target.value);
-    }
-    useEffect(() => {
-        filterHandeler();
-    }, [todos, status]);
-    const toggleHandeler = () => {
-        setToggler(!toggler);
-    }
-    return ( 
-        <div className="app">
-           <Header onToggle={toggleHandeler} Select={selectHandeler} toggler={toggler}  />
-           {toggler &&  <Form onAdd={addTodos} />}
-           {todos.length > 0 ? <TodoList todos={todos} setTodos={setTodos}  filtters={filtters} /> : "It is empty Add some Todos"}
-            <Footer />
+    return (
+        <div>
+            <Navbar />
+            <CityList />
         </div>
     )
 }
---
+++ src/Component/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return(
+        <nav>
+            <h2>City Tours</h2>
+            <div>
+                <li>Home</li>
+                <li>Tours</li>
+                <li>About-Us</li>
+            </div>
+        </nav>
+    )
+}
+export default Navbar;
